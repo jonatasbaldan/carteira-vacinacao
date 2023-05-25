8efaf20bfe8e222c799fc77cfba18fd053a376d4
Jonatas Baldan <jonatasbzc@gmail.com>
2023-05-25 01:54:01 -0300

    Criado o formulário de login.

--- website/views.py
+++ website/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == 'POST':
+        cpf = request.POST['cpf']
+        password = request.POST['password']
+        user = authenticate(request, username=cpf, password=password)
 
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Cpf ou senha incorretos.", extra_tags='alert-warning')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
