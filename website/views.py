ba3c55bbccb67dc0c402708f7a21db317510f078
Jonatas Baldan <jonatasbzc@gmail.com>
2023-05-25 02:02:24 -0300

    Refatorado o método home, atribuindo a lógica d login para um método separado.

--- website/views.py
+++ website/views.py
@@ -2,25 +2,27 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+
 # Create your views here.
 
 def home(request):
     if request.method == 'POST':
-        cpf = request.POST['cpf']
-        password = request.POST['password']
-        user = authenticate(request, username=cpf, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, "Cpf ou senha incorretos.", extra_tags='alert-warning')
-            return redirect('home')
+        return login_user(request)
     else:
         return render(request, 'home.html', {})
 
+
 def login_user(request):
-    pass
+    cpf = request.POST['cpf']
+    password = request.POST['password']
+    user = authenticate(request, username=cpf, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('home')
+    else:
+        messages.error(request, "Cpf ou senha incorretos.", extra_tags='alert-warning')
+        return redirect('home')
+
 
 def logout_user(request):
     pass
