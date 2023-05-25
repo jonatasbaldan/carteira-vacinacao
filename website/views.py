03c98d2fc45ef874478bbe46548040e6bd4bf780
Jonatas Baldan <jonatasbzc@gmail.com>
2023-05-24 22:54:03 -0300

    Criado o painel admin e alterado o time zone.

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {})
+
